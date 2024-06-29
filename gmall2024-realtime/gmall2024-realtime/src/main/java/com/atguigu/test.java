ea71b258374301b3e5e56226726f5083745137dc
wxguo <15318246086@163.com>
2024-06-29 10:13:48 +0800

    init

---
+++ gmall2024-realtime/gmall2024-realtime/src/main/java/com/atguigu/test.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author name 婉然从物
+ * @create 2024-04-21 19:53
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
